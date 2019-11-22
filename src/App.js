798f927833a522131a04c8db04be0db503d2fa52
Nitesh Devnath <53181176+niteshdeveloper123@users.noreply.github.com>
2019-11-22 18:06:58 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -8,34 +8,50 @@ const App = () => {
   const APP_KEY = 'af7edad56e076b97764ce9d25ddacf31';
 
   const [recipes, setRecipes] =  useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState("chicken")
 
   useEffect(()=> {
       getRecipes();
-  }, [])
+  }, [query])
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`); 
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`); 
     const data = await response.json();
-    // console.log(data);
+    console.log(data);
 
     setRecipes(data.hits);
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    // console.log(search);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe 
-          key={recipe.recipe.label}
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image} />
-      ))}
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe 
+            key={recipe.recipe.totalWeight}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image} 
+            ingredients={recipe.recipe.ingredients}
+            />
+        ))}
+      </div>
     </div>
   )
 }
